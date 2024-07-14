42554fd0e71ac4038b9711c463c155fb3f46d4b0
Donald Sutherland <donald@sutherland.id.au>
2024-07-14 18:48:15 +0800

    Initial - testing with a websocket server

---
+++ test_ws_server/test_ws_server.py
@@ -0,0 +1,22 @@
+import asyncio
+from websockets.server import serve, WebSocketServerProtocol
+
+
+async def echo(websocket: WebSocketServerProtocol):
+	print(f"Someone connected: ")
+	
+	async def auto_send_loop():
+		x = 1
+		while websocket.open:
+			await websocket.send(f"Ping! * {x}")
+			x += 1
+			await asyncio.sleep(10)
+	looping_task = asyncio.create_task(auto_send_loop())
+	async for message in websocket:
+		print(message)
+		await websocket.send(message)
+
+
+async def main():
+	async with serve(echo):
+		await asyncio.Future()
